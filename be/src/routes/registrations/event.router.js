f90bf612ba4303fd9da77d6ea82c3990db669fd0
mitz <mit0612203@gmail.com>
2025-10-21 01:48:38 +0700

    update

---
+++ be/src/routes/registrations/event.router.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { asyncHandler } from '@/utils/helpers'
+import requireRegistrationAuthentication from '@/app/middleware/registrations/require-authentication'
+import * as eventController from '@/app/controllers/organizer/event.controller'
+
+const eventRouter = Router()
+
+// All routes require registration authentication
+eventRouter.use(asyncHandler(requireRegistrationAuthentication))
+
+// Mirror GET endpoints from organizer events for registrations context
+eventRouter.get('/', asyncHandler(eventController.listEvents))
+eventRouter.get('/search', asyncHandler(eventController.searchEvents))
+eventRouter.get('/nearby', asyncHandler(eventController.getNearbyEvents))
+eventRouter.get('/pin/:pinCode', asyncHandler(eventController.getEventByPinCode))
+eventRouter.get('/:id', asyncHandler(eventController.getEventById))
+
+export default eventRouter
+
--- be/src/routes/registrations/index.js
+++ be/src/routes/registrations/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import authRouter from './auth.router'
 import userRouter from './user.router'
+import eventRouter from './event.router'
 
 const registrationsRouter = Router()
 
 registrationsRouter.use('/auth', authRouter)
+registrationsRouter.use('/events', eventRouter)
 registrationsRouter.use('/users', userRouter)
 
-export default registrationsRouter 
+export default registrationsRouter
